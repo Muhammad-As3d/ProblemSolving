ddf8e18352eee77fe0b7f3204cf18efe69965fa4
Muhammad Asaad <meshmuhammadasaad@gmail.com>
2026-05-24 15:52:17 +0300

    Add Two Numbers

---
+++ ProblemSolving/4_AddTwoNumbers/AddTwoNumbers.cs
@@ -0,0 +1,48 @@
+﻿namespace ProblemSolving._4_AddTwoNumbers;
+
+public static class AddTwoNumbers
+{
+    public static ListNode AddTwoNumbersMethod(ListNode l1, ListNode l2)
+    {
+        ListNode dummy = new ListNode(0);
+
+        ListNode current = dummy;
+
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0)
+        {
+            int x = l1 != null ? l1.val : 0;
+            int y = l2 != null ? l2.val : 0;
+
+            int sum = x + y + carry;
+
+            carry = sum / 10;
+
+            current.next = new ListNode(sum % 10);
+
+            current = current.next;
+
+            if (l1 != null)
+                l1 = l1.next;
+
+            if (l2 != null)
+                l2 = l2.next;
+        }
+
+        return dummy.next;
+    }
+}
+
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
