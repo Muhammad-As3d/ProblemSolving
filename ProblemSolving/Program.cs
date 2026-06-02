473b26dbbb0158f27044258c54a3aee47e76e415
Muhammad Asaad <muhammad.as3d@gmail.com>
2026-06-02 16:23:32 +0300

    IsPalindrome Problem

--- ProblemSolving/Program.cs
+++ ProblemSolving/Program.cs
@@ -25,4 +25,23 @@
 
 //Console.WriteLine(resu);
 
- 
+
+//IsPalindrome Problem
+Console.WriteLine(IsPalindrome("saas"));
+static bool IsPalindrome(string s)
+{
+    int i = 0;
+    int j = s.Length - 1;
+
+    while (i < j)
+    {
+        if (s[i] != s[j])
+            return false;
+
+        i++;
+        j--;
+    }
+
+    return true;
+
+}
