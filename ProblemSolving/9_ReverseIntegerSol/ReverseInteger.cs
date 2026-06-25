56c06a8961f263db204e80436d48d25f28836e1a
Muhammad Asaad <muhammad.as3d@gmail.com>
2026-06-25 16:53:44 +0300

    ReverseInteger

---
+++ ProblemSolving/9_ReverseIntegerSol/ReverseInteger.cs
@@ -0,0 +1,22 @@
+﻿namespace ProblemSolving._9_ReverseIntegerSol;
+
+public static class ReverseInteger
+{
+    public static int Reverse(int x)
+    {
+        int result = 0;
+
+        while (x != 0)
+        {
+            int digit = x % 10;
+            x /= 10;
+
+            if (result < int.MinValue / 10 || (result == int.MinValue / 10 && digit < -8))
+                return 0;
+
+            result = result * 10 + digit;
+        }
+
+        return result;
+    }
+}
